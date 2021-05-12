45969c68282c3d01c016ccc1669abcb336daaca3
Vegard Stikbakke <vegard.stikbakke@gmail.com>
2021-05-12 08:02:08 +0200

    Add kvd command which runs socket server

---
+++ cmd/kvd/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"kv/socket"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func main() {
+	s := socket.NewServer()
+	if err := s.Open(); err != nil {
+		log.Printf("%v", err)
+		os.Exit(1)
+	}
+	defer s.Close()
+	log.Printf("Listening on %s", s.Address())
+	var interruptChannel = make(chan os.Signal, 1)
+	signal.Notify(interruptChannel, os.Interrupt, syscall.SIGTERM, syscall.SIGABRT, syscall.SIGINT)
+	sig := <-interruptChannel
+	log.Println("Got interrupt signal: ", sig.String())
+	log.Println("Shutting down server")
+}
